84124ac97f710056ec26346beeb5adf6c834b06d
ychen <ychen@eproassociates.com>
2026-01-27 10:18:13 -0500

    Cloudfare compatibility

--- tina/config.tsx
+++ tina/config.tsx
@@ -1,5 +1,4 @@
 import { defineConfig } from "tinacms";
-import nextConfig from '../next.config'
 
 import Post from "./collection/post";
 import Global from "./collection/global";
@@ -7,33 +6,32 @@ import Author from "./collection/author";
 import Page from "./collection/page";
 import Tag from "./collection/tag";
 
-const config = defineConfig({
+export default defineConfig({
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID!,
-  branch:
-    process.env.NEXT_PUBLIC_TINA_BRANCH! || // custom branch env override
-    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF! || // Vercel branch env
-    process.env.HEAD!, // Netlify branch env
   token: process.env.TINA_TOKEN!,
+
+  // Cloudflare Pages branch detection
+  branch:
+    process.env.NEXT_PUBLIC_TINA_BRANCH || // manual override
+    process.env.CF_PAGES_BRANCH || // Cloudflare Pages
+    "main",
+
   media: {
-    // If you wanted cloudinary do this
-    // loadCustomStore: async () => {
-    //   const pack = await import("next-tinacms-cloudinary");
-    //   return pack.TinaCloudCloudinaryMediaStore;
-    // },
-    // this is the config for the tina cloud media store
     tina: {
       publicFolder: "public",
       mediaRoot: "uploads",
     },
   },
+
   build: {
-    publicFolder: "public", // The public asset folder for your framework
-    outputFolder: "admin", // within the public folder
-    basePath: nextConfig.basePath?.replace(/^\//, '') || '', // The base path of the app (could be /blog)
+    publicFolder: "public",
+    outputFolder: "admin",
+
+    // Avoid importing next.config.js on Cloudflare
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH?.replace(/^\//, "") || "",
   },
+
   schema: {
     collections: [Page, Post, Author, Tag, Global],
   },
 });
-
-export default config;
