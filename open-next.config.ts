9fed75401388e613b38c58ec3c7ef76a1ecbebe0
ychen-epro <ychen@eproassociates.com>
2026-02-06 07:25:56 -0800

    Create  open-next.config.ts

---
+++ open-next.config.ts
@@ -0,0 +1,15 @@
+import type { OpenNextConfig } from '@opennextjs/cloudflare';
+
+const config: OpenNextConfig = {
+  default: {
+    override: {
+      wrapper: 'cloudflare-node',
+      converter: 'edge',
+      incrementalCache: 'cloudflare-kv',
+      tagCache: 'cloudflare-kv',
+      queue: 'cloudflare-queue',
+    },
+  },
+};
+
+export default config;
